27470d56227b5b3a9f2a5bc7e1869e1db320e0ec
Manuchimso Oliver <manuchimoliver779@gmail.com>
2022-12-18 22:29:52 +0100

    Added comments

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -14,31 +14,37 @@ import { UpdateUserDto } from './dto/update-user.dto';
 import { UserQueryDto } from './dto/query.dto';
 import { isEmpty } from '../utils';
 
+//users controller
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) { }
 
+//create a new user
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
 
+//get all users
   @Get()
   findAll(@Query() query: UserQueryDto) {
 
     return this.usersService.findAll(isEmpty(query) ? null : query);
   }
 
+//get user by id
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.usersService.findOne(id);
   }
 
+//Update user
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(id, updateUserDto);
   }
 
+//delete user
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.usersService.remove(id);
